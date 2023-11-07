f862c928e630d4982a739bcd0451dcc7f95792fa
Emily <83665046+ThauanyCelestino@users.noreply.github.com>
2023-11-07 17:10:25 -0400

    1,2,3,4 procedures

--- BD_Rodoviaria - 2.0.sql
+++ BD_Rodoviaria - 2.0.sql
@@ -147,4 +147,157 @@ foreign key (id_pas_fk) references Passagem (id_pas)
 
 #INICIE A PARTIR DAQUI SUA LISTA DE EXERCÍCIOS
 
+#TAREFA 1
+
+# AÇÃO: Inserir Estado.
+DELIMITER $$
+Create Procedure InserirEstado(
+   NomeEst varchar(300),
+   Sigla varchar(4)
+)
+BEGIN
+    declare nome_teste varchar(200);
+	set nome_teste = (Select nome_est from Estado where nome_est = NomeEst);
+    
+	IF nome_teste is null then      #1º VERIFICAÇÃO
+        Insert into Estado (nome_est, sigla_est) values (NomeEst, Sigla);
+        Select concat('Estado', NomeEst, 'foi inserido com sucesso!') as Confirmação; 
+    ELSE
+        Select 'Estado já existe! Digite uma informação válida.' as Alerta; 
+    END IF;
+    
+END 
+$$ DELIMITER ;
+
+#CHAMADA 
+Call InserirEstado('Rondônia', 'RO');
+Call InserirEstado('Mato Grosso', 'MT');
+Call InserirEstado('Acre', 'AC');
+Call InserirEstado('Amazonas', 'AM');
+Call InserirEstado('Mato Grosso do Sul', 'MS');
+Call InserirEstado('Pará', 'PA');
+Call InserirEstado('Tocantins', 'TO');
+Call InserirEstado('Roraima', 'RR');
+Call InserirEstado('Amapá', 'AP');
+Call InserirEstado('Maranhão', 'MA');
+
+# TAREFA 2
+
+# AÇÃO: Inserir Cidade.
+DELIMITER $$
+Create Procedure InserirCidade (
+CidNome varchar (200),
+estado_fk int)
+BEGIN
+declare teste_nome, teste_fk varchar (300);
+
+set teste_nome = (Select nome_cid from cidade where (nome_cid = CidNome));
+set teste_fk = (Select id_est from estado where (id_est = estado_fk));
+
+	IF (teste_nome = '') or (teste_nome is null) then 	  #1º VERIFICAÇÃO    
+		IF (teste_fk is not null) then		 #2º VERIFICAÇÃO
+			Insert into cidade (nome_cid, id_est_fk) values (CidNome, estado_fk);
+			Select concat('A Cidade ', CidNome, ' foi salva com sucesso!') as Confirmação;
+		ELSE
+			Select 'A FK informada não existe na tabela Estado' as Alerta;
+		END IF;
+    ELSE
+			Select 'Cidade já existe! Digite uma informação válida.' as Alerta;
+    END IF;
+    
+END;
+$$ DELIMITER ;
+
+#CHAMADA
+Call InserirCidade('Porto Velho', 1);
+Call InserirCidade('Ji-Paraná', 1);
+Call InserirCidade('Cacoal', 1);
+Call InserirCidade('Guajará-Mirim', 1);
+Call InserirCidade('Ariquemes', 1);
+Call InserirCidade('Vilhena', 1);
+Call InserirCidade('Pimenta Bueno', 1);
+Call InserirCidade('Ouro Preto do Oeste', 1);
+Call InserirCidade('Rolim de Moura', 1);
+Call InserirCidade('Candeias do Jamari', 1);
+
+Call InserirCidade('Cuiabá', 2);
+Call InserirCidade('Várzea Grande', 2);
+Call InserirCidade('Rondonópolis', 2);
+
+Call InserirCidade('Rio Branco', 3);
+Call InserirCidade('Cruzeiro do Sul', 3);
+
+Call InserirCidade('Manaus', 4);
+Call InserirCidade('Itacoatiara', 4);
+Call InserirCidade('Parintins', 4);
+
+Call InserirCidade('Campo Grande', 5);
+Call InserirCidade('Dourados', 5);
+
+#Tarefa 3
+
+#AÇÃO: Inserir Endereço
+DELIMITER $$
+Create Procedure InserirEndereço (
+   Rua varchar (300),
+   Numero int,
+   Bairro varchar (100),
+   Cep varchar (100),
+   cidade_fk int
+)
+BEGIN
+declare teste_fk int;
+
+set teste_fk = (Select id_cid from Cidade where (id_cid = cidade_fk));
+
+	IF (teste_fk is not null) then   # 1ª VERIFICAÇÃO
+		Insert into Endereço (rua_end, numero_end, bairro_end, cep_end, id_cid_fk) 
+		values (Rua, Numero, Bairro, CEP, cidade_fk);
+			Select concat('O Endereço foi salvo com sucesso!') as Confirmação;
+	ELSE
+			Select 'A FK informada não existe na tabela Endereço' as Alerta;
+    END IF;
+
+END;
+$$ DELIMITER ;
+
+#CHAMADA
+Call InserirEndereço('Rua A', 123, 'Bairro A', 'CEP 12345-678', 1);
+Call InserirEndereço('Rua B', 456, 'Bairro B', 'CEP 23456-789', 2);
+Call InserirEndereço('Rua C', 789, 'Bairro C', 'CEP 34567-890', 3);
+Call InserirEndereço('Rua D', 101, 'Bairro D', 'CEP 45678-901', 4);
+Call InserirEndereço('Rua E', 234, 'Bairro E', 'CEP 56789-012', 5);
+Call InserirEndereço('Rua F', 567, 'Bairro F', 'CEP 67890-123', 6);
+Call InserirEndereço('Rua G', 890, 'Bairro G', 'CEP 78901-234', 7);
+Call InserirEndereço('Rua H', 111, 'Bairro H', 'CEP 89012-345', 8);
+Call InserirEndereço('Rua I', 222, 'Bairro I', 'CEP 90123-456', 9);
+Call InserirEndereço('Rua J', 333, 'Bairro J', 'CEP 01234-567', 10);
+
+# Tarefa 4
+DELIMITER $$ 
+Create Procedure InserirSexo ( 
+nomeSex varchar(255) 
+)
+   BEGIN
+   declare sexo_teste varchar(200);
+   
+   set sexo_teste = (Select nome_sex from Sexo where nome_sex = nomeSex);
+
+   IF nomeSex <> '' then 	#1ª VERIFICAÇÃO 
+    IF sexo_teste is null then 	#2ª VERIFICAÇÃO
+        Insert into Sexo (nome_sex) values (nomeSex);
+        Select 'Sexo inserido com sucesso!' AS Mensagem;
+    ELSE
+        Select 'Sexo já existe! Digite uma informação válida.' AS Mensagem;
+	END IF;
+	ELSE
+		Select 'Campo sexo não pode ser vazio!' AS Mensagem;
+	END IF;
+
+END 
+$$ DELIMITER ;
+
+#CHAMADA
+Call InserirSexo('Masculino');
+Call InserirSexo('Feminino');
 
