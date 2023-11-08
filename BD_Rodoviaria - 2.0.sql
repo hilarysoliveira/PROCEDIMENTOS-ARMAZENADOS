c947a7461d676969489d610f3fa0505f68191aa5
Emily <83665046+ThauanyCelestino@users.noreply.github.com>
2023-11-08 01:19:12 -0400

    completo

--- BD_Rodoviaria - 2.0.sql
+++ BD_Rodoviaria - 2.0.sql
@@ -1,5 +1,5 @@
 #Sistema para Empresa de Transporte Rodoviario
-
+# EMILY DAMACENO, HILARY SOUZA, THAUANY CELESTINO.
 create database bd_rodoviaria_2;
 use bd_rodoviaria_2;
 
@@ -157,9 +157,9 @@ BEGIN
     
 	IF (teste_nome is null) then  #1º VERIFICAÇÃO 
         Insert Into Estado (nome_est, sigla_est) values (NomeEst, Sigla);
-        Select concat('O estado ', NomeEst, ' foi inserido com sucesso!') as Confirmação; 
+        Select concat('O estado ', NomeEst, ' foi inserido com sucesso!') as CONFIRMAÇÃO; 
     ELSE
-        Select concat('O estado ', NomeEst,' já existe! Digite outro estado.') as Alerta; 
+        Select concat('O estado ', NomeEst,' já existe! Digite outro estado.') as ALERTA; 
     END IF;
     
 END 
@@ -191,12 +191,12 @@ BEGIN
 	IF (teste_nome is null) then 	  #1º VERIFICAÇÃO    
 		IF (teste_fk is not null) then		 #2º VERIFICAÇÃO
 			Insert into Cidade (nome_cid, id_est_fk) values (CidNome, estado_fk);
-			Select concat('A Cidade ', CidNome, ' foi inserida com sucesso!') as Confirmação;
+			Select concat('A Cidade ', CidNome, ' foi inserida com sucesso!') as CONFIRMAÇÃO;
 		ELSE
-			Select 'O código do estado informado não existe na tabela estado.' as Alerta;
+			Select 'O estado inserido não existe!' as ALERTA;
 		END IF;
     ELSE
-			Select concat('A cidade ', CidNome, ' já existe! Digite outra cidade.') as Alerta;
+			Select concat('A cidade ', CidNome, ' já existe! Digite outra cidade.') as ALERTA;
     END IF;
     
 END;
@@ -234,7 +234,7 @@ Call InserirCidade('Campo Grande', 5);
 Call InserirCidade('Dourados', 5);
 
 
-#Tarefa 3
+#TAREFA 3
 #AÇÃO: Inserir Endereço
 DELIMITER $$
 Create Procedure InserirEndereço (
@@ -251,9 +251,9 @@ set teste_fk = (Select id_cid from Cidade where (id_cid = cidade_fk));
 	IF (teste_fk is not null) then   # 1ª VERIFICAÇÃO
 		Insert into Endereço (rua_end, numero_end, bairro_end, cep_end, id_cid_fk) 
 		values (Rua, Numero, Bairro, CEP, cidade_fk);
-			Select 'O Endereço foi salvo com sucesso!' as Confirmação;
+			Select 'O Endereço foi salvo com sucesso!' as CONFIRMAÇÃO;
 	ELSE
-			Select 'O código da cidade informado não existe na tabela cidade' as Alerta;
+			Select 'A cidade fornecida não existe!' as ALERTA;
     END IF;
 
 END;
@@ -272,7 +272,7 @@ Call InserirEndereço('Rua dos Sonhos', 579, 'Bairro da Colina', '34567-898', 9)
 Call InserirEndereço('Alameda da Esperança', 864, 'Bairro do Lago', '34567-899', 10);
 
 
-# Tarefa 4
+# TAREFA 4
 # AÇÃO: Inserir Sexo
 DELIMITER $$ 
 Create Procedure InserirSexo (nomeSex varchar(255))
@@ -283,12 +283,12 @@ BEGIN
    IF nomeSex <> '' then 	#1ª VERIFICAÇÃO 
 	 IF sexo_teste is null then 	#2ª VERIFICAÇÃO
         Insert into Sexo (nome_sex) values (nomeSex);
-			Select concat('O Sexo ', nomeSex, ' foi inserido com sucesso!') AS Confirmação;
+			Select concat('O Sexo ', nomeSex, ' foi inserido com sucesso!') AS CONFIRMAÇÃO;
      ELSE
-			Select concat('Sexo ', nomeSex, ' já existe! Digite outro sexo.') AS Alerta;
+			Select concat('Sexo ', nomeSex, ' já existe! Digite outro sexo.') AS ALERTA;
 	 END IF;
 	ELSE
-			Select 'O campo Sexo não pode ser vazio!' AS Alerta;
+			Select 'Preencha o campo Sexo!' AS ALERTA;
 	END IF;
 
 END 
@@ -299,3 +299,207 @@ Call InserirSexo('Feminino');
 Call InserirSexo('Masculino');
 
 
+# TAREFA 5 
+#AÇÃO: Inserir Telefone
+DELIMITER $$
+Create procedure InserirTelefone ( celular varchar(100), casa varchar(200), trabalho varchar(100))
+BEGIN 
+
+	IF ( celular <> '' ) then	 # 1º VERIFICAÇÃO
+		Insert into telefone values (null, celular, casa, trabalho);
+			Select 'Os contatos foram salvos com secesso!' as CONFIRMAÇÃO;
+	ELSE 
+			Select 'Preencha o campo Celular!' as ALERTA;
+	END IF;
+END;
+$$ DELIMITER ;
+
+#CHAMADA
+Call InserirTelefone ('21369874123' , '7456662666', '96666478787');
+Call InserirTelefone ('55166456664', '555454555', '5122233121');
+Call InserirTelefone ('25455665444', '99741288988', '555198878774');
+Call InserirTelefone ('35655455654', '545565552365', '458962315847');
+Call InserirTelefone ('56984712340', '31788970214', '11023074986');
+
+
+# TAREFA 6
+# AÇÃO: Inserir Cliente
+DELIMITER $$
+Create Procedure InserirCliente (Nome varchar(200), EstadoCivil varchar(200), CPF varchar(200),
+RG varchar(200), DataNasc date, Sexo_fk int, Endereco_fk int, Telefone_fk int
+)
+BEGIN
+
+declare sexo_teste int;
+declare endereco_teste int;
+declare telefone_teste int;
+
+set sexo_teste = (Select id_sex from Sexo where (id_sex = Sexo_fk));
+set endereco_teste = (Select id_end from Endereço where (id_end = Endereco_fk));
+set telefone_teste = (Select id_tel from Telefone where (id_tel = Telefone_fk));
+
+	IF (sexo_teste is not null) then 	#1ª VERIFICAÇÃO
+		IF (endereco_teste is not null) then  #2ª VERIFICAÇÃO
+			IF (telefone_teste is not null) then  #3ª VERIFICAÇÃO
+					Insert into Cliente (nome_cli, estadocivil_cli, cpf_cli, rg_cli, datanasc_cli, id_sex_fk, id_end_fk, id_tel_fk) 
+					values (Nome, EstadoCivil, CPF, RG, DataNasc, Sexo_fk, Endereco_fk, Telefone_fk);
+					Select concat('O Cliente ', Nome, ' foi salvo com sucesso!') as CONFIRMAÇÃO;
+			ELSE 
+				Select 'O telefone informado não existe' as ALERTA;
+			END IF;
+		ELSE
+			Select 'O endereço informado não existe' as ALERTA;
+		END IF;
+	ELSE
+		Select 'O sexo informado não existe!' as ALERTA;
+	END IF;
+END;
+$$ DELIMITER ;
+ 
+#CHAMADA
+Call InserirCliente('Fernanda Martins', 'Solteira', '55566677788', 'RG13579', '1993-07-20', 2, 6, 6);
+Call InserirCliente('Ricardo Santos', 'Casado', '77788899900', 'RG24680', '1979-11-03', 1, 7, 7);
+Call InserirCliente('Juliana Pereira', 'Casada', '22233344455', 'RG98765', '1988-04-15', 2, 8, 8);
+Call InserirCliente('Gustavo Almeida', 'Solteiro', '88899900011', 'RG23456', '1991-12-08', 1, 9, 9);
+Call InserirCliente('Larissa Silva', 'Divorciada', '11112223334', 'RG87654', '1987-10-25', 2, 10, 10);
+
+
+# TAREFA 7
+# AÇÃO: Inserir Departamento
+DELIMITER $$
+Create Procedure InserirDepartamento (nome varchar(300), descricao varchar(400))
+BEGIN
+
+ IF ( nome <> '') then	 # 1º VERIFICAÇÃO
+	IF ( descricao <> '') then 	# 2º VERIFICAÇÃO
+		Insert into departamento values (null, nome, descricao);
+		Select concat('O Departamento ', nome,' foi inserido com sucesso!') as CONFIRMAÇÃO;
+    ELSE
+		Select 'Peencha o campo Descrição!'  as ALERTA;
+	END IF;
+ELSE
+		Select 'Peencha o campo Nome!'  as ALERTA;
+END IF;
+END;
+$$ DELIMITER ;
+ 
+#CHAMADA
+Call InserirDepartamento('Vendas de Passagens', 'Responsável por atrair passageiros, promover destinos, definir preços de passagens e gerenciar estratégias de marketing e vendas.');  
+Call InserirDepartamento('Operações', 'Responsável por planejar e coordenar as operações diárias da frota de ônibus.');
+Call InserirDepartamento('Finanças e Contabilidade', 'Lida com todas as questões financeiras da empresa, incluindo controle de despesas, orçamento e contabilidade.');
+
+# TAREFA 8
+# AÇÃO: Inserir Funcionário
+DELIMITER $$
+Create Procedure InserirFuncionario ( Nome varchar (200), CPF varchar(200), RG varchar (200), DataNasc date,
+Salario double, Funcao varchar (50), Sexo_fk int, Departamento_fk int, Endereco_fk int, Telefone_fk int
+)
+BEGIN
+	declare sexo_teste int;
+	declare departamento_teste int;
+	declare endereco_teste int;
+	declare telefone_teste int;
+	declare vendaDEP_teste int;
+
+	set sexo_teste = (Select id_sex from Sexo where (id_sex = Sexo_fk));
+	set departamento_teste = (Select id_dep from Departamento where (id_dep = Departamento_fk));
+	set endereco_teste = (Select id_end from Endereço where (id_END = Endereco_fk));
+	set telefone_teste = (Select id_tel from Telefone where (id_tel = Telefone_fk));
+	set vendaDEP_teste = (Select id_dep from Departamento where (nome_dep LIKE 'Vendas de Passagens'));
+
+IF (sexo_teste is not null) then  # 1º VERIFICAÇÃO
+    IF (departamento_teste is not null) then # 2º VERIFICAÇÃO
+        IF (endereco_teste is not null) then # 3º VERIFICAÇÃO
+            IF (telefone_teste is not null) then # 4º VERIFICAÇÃO
+                IF (Funcao LIKE 'Vendedor') then # 5º VERIFICAÇÃO
+                    IF (Departamento_fk <> vendaDEP_teste) then # 5.1º VERIFICAÇÃO
+                        Select 'Um Vendedor só pode ser inserido no departamento de Vendas de Passagens' as ALERTA;
+                    ELSE
+                        Insert into Funcionario (nome_func, cpf_func, rg_func, datanasc_func, salário_func, função_func, id_sex_fk, id_dep_fk, id_end_fk, id_tel_fk) 
+                        values (Nome, CPF, RG, DataNasc, Salario, Funcao, Sexo_fk, Departamento_fk, Endereco_fk, Telefone_fk);
+                        Select concat('O Funcionário ', Nome ,' foi salvo com sucesso!') as CONFIRMAÇÃO;
+                    END IF;
+                ELSE
+                    Insert into Funcionario (nome_func, cpf_func, rg_func, datanasc_func, salário_func, função_func, id_sex_fk, id_dep_fk, id_end_fk, id_tel_fk) 
+                    values (Nome, CPF, RG, DataNasc, Salario, Funcao, Sexo_fk, Departamento_fk, Endereco_fk, Telefone_fk);
+                    Select concat('O Funcionário ', Nome ,' foi salvo com sucesso!') as CONFIRMAÇÃO;
+                END IF;
+            ELSE 
+                Select 'O telefone informado não existe' as ALERTA;
+            END IF;
+        ELSE 
+            Select 'O endereço informado não existe' as ALERTA;
+        END IF;
+    ELSE 
+        Select 'O departamento informado não existe' as ALERTA;
+    END IF;
+ELSE 
+    Select 'O sexo informado não existe' as ALERTA;
+END IF;
+
+END;
+$$ DELIMITER ;
+
+#CHAMADA
+Call InserirFuncionario ('Pedro Souza', '555.666.777-88', '55.666.777-8', '1999-10-11', 3200.00, 'Motorista', 1, 2, 5, 5);
+Call InserirFuncionario ('Ana Pereira', '666.777.888-99', '66.777.888-9', '2000-11-12', 2900.00, 'Atendente', 2, 3, 2, 3);
+Call InserirFuncionario ('Marcos Silva', '777.888.999-00', '77.888.999-0', '2001-12-13', 3100.00, 'Controlador', 1, 1, 7, 5);
+Call InserirFuncionario ('Isabel Santos', '888.999.000-11', '88.999.000-1', '2002-01-14', 2800.00, 'Limpeza', 2, 1, 1, 2);
+
+# TAREFA 9
+# AÇÃO: Inserir Onibus
+DELIMITER $$
+Create Procedure InserirOnibus (Modelo varchar(200), Marca varchar(200),Placa varchar(200), Tipo varchar(200)
+)
+BEGIN
+	IF ((Modelo = 'Amazon Bus Premium' and Tipo = 'Executive') or (Modelo = 'Amazon Bus Leito' and Tipo = 'Confort')) then  # 1º VERIFICAÇÃO
+		Insert into Onibus (modelo_oni, marca_oni, placa_oni, tipo_oni) values (Modelo, Marca, Placa, Tipo);
+			Select concat('O Ônibus foi salvo com sucesso!') as CONFIRMAÇÃO;
+	ELSE
+		Select 'Digite um modelo e tipo de ônibus válidos: "Amazon Bus Premium e Executive" ou "Amazon Bus Leito  e Confort".' as ALERTA;
+    END IF;
+END;
+$$ DELIMITER ;
+
+#CHAMADA
+Call InserirOnibus ('Amazon Bus Leito', 'Volvo', 'DEF-5678', 'Executive');
+Call InserirOnibus ('Amazon Bus Premium', 'Iveco', 'MNO-9012', 'Executive');
+Call InserirOnibus ('Amazon Bus Leito', 'MAN', 'PQR-3456', 'Confort');
+Call InserirOnibus ('Amazon Bus Premium', 'MercedesBenz', 'ABC-1234', 'Executive');
+
+# TAREFA 10
+# AÇÃO: Inserir Poltrona
+DELIMITER $$
+Create Procedure InserirPoltronas(Onibusfk int)
+BEGIN
+ declare tipo_teste varchar(100);
+ declare contador int;
+ declare limite int;
+
+ select tipo_oni into tipo_teste from Onibus where id_oni = Onibusfk;
+    
+ IF (tipo_teste LIKE 'Executive' OR tipo_teste LIKE 'Confort') then # 1º VERIFICAÇÃO
+	IF tipo_teste = 'Executive' then
+		set limite = 48;
+	ELSE
+		set limite = 58;
+	END IF;
+
+	set contador = 1;
+	WHILE contador <= limite DO
+		Insert into Poltrona (número_pol, situação_pol, id_oni_fk) values (contador, 'Livre', Onibusfk);
+		set contador = contador + 1;
+	END WHILE;
+		select 'As poltronas do ônibus foram inseridas com sucesso!' AS CONFIRMAÇÃO;
+ELSE
+	select 'Digite um tipo de ônibus válido: "Executive" ou "Confort".' AS ALERTA;
+END IF;
+END;
+$$
+DELIMITER ;
+
+#CHAMADA
+Call InserirPoltronas (1);
+Call InserirPoltronas (2);
+Call InserirPoltronas (3);
+Call InserirPoltronas (4);
